ade569173f1339b3ffc3c14da879dc9c5b493f79
raahii <piyo56.net@gmail.com>
2020-05-30 23:56:56 +0900

    Add mysql-exporter.

--- master/init.sql
+++ master/init.sql
@@ -1,4 +1,6 @@
+/* proxysql user */
 CREATE USER IF NOT EXISTS 'monitor'@'%' IDENTIFIED WITH sha256_password BY 'monitor';
+/* slave user */
 CREATE USER IF NOT EXISTS 'slave_user'@'%' IDENTIFIED BY 'password';
 GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' WITH GRANT OPTION;
 
