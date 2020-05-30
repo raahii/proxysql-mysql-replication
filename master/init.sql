323327eb0080d9ca89a6051760301ef64caceb27
raahii <piyo56.net@gmail.com>
2020-05-30 23:57:11 +0900

    Add prometheus, graphana.

--- master/init.sql
+++ master/init.sql
@@ -1,5 +1,10 @@
 /* proxysql user */
 CREATE USER IF NOT EXISTS 'monitor'@'%' IDENTIFIED WITH sha256_password BY 'monitor';
+
+/* mysql exporter user */
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'password' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+
 /* slave user */
 CREATE USER IF NOT EXISTS 'slave_user'@'%' IDENTIFIED BY 'password';
 GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' WITH GRANT OPTION;
